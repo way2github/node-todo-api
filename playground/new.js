1ee9bacf8580f998391dab1bd08c177217a8b6c3
way2github <mr.ayan.mitra@gmail.com>
2018-08-07 15:03:53 +0530

    CRD added

--- playground/new.js
+++ playground/new.js
@@ -6,27 +6,58 @@ MongoClient.connect('mongodb://127.0.0.1:27017/School', (err, db) => {
         return console.log('Unable to connect');
     }
     console.log('Connected');
+    // Delete all
+    db.collection('Students').deleteMany();
+
+    // Create
+    db.collection('Students').insertMany([
+        { name: 'Ronav', gender: 'M', secondLanguage: 'Hindi' },
+        { name: 'Arnesh', gender: 'M', secondLanguage: 'Hindi' },
+        { name: 'Avery', gender: 'F', secondLanguage: 'Bengali' },
+        { name: 'Debangi', gender: 'F', secondLanguage: 'Bengali' }],
+        (err, res) => {
+            if (err) { return console.log('Unable to insert'); }
+            console.log(JSON.stringify(res.ops, undefined, 2));
+            console.log(JSON.stringify(res.ops[0]._id.getTimestamp()));
+            console.log('### Create done ###');
+        });
+
+
+    // Read
     db.collection('Students')
         .find({ secondLanguage: 'Hindi' })
         .toArray()
         .then(
             (docs) => {
                 console.log(JSON.stringify(docs, undefined, 2));
+                console.log('### Read-1 done ###');
             }, (err) => {
                 console.log('Unable to find ', err);
             });
-    /*
-    db.collection('Students').insertOne({
-        name: 'Ronav',
-        rollnum: 28,
-        age: 6,
-        sex: 'M',
-        secondLanguage: 'Hindi'
-    }, (err, res) => {
-        if (err) { return console.log('Unable to insert'); }
-        console.log(JSON.stringify(res.ops, undefined, 2));
-        console.log(JSON.stringify(res.ops[0]._id.getTimestamp()))
-    });
-    */
+
+
+    // Delete
+    db.collection('Students').findOneAndDelete({ secondLanguage: 'Bengali' })
+        .then((res) => {
+            console.log(JSON.stringify(res.value.name, undefined, 2));
+            console.log('### Delete done ###');
+        },
+            (err) => { console.log(err); });
+
+
+    // Read
+    db.collection('Students')
+        .find()
+        .toArray()
+        .then(
+            (docs) => {
+                console.log(JSON.stringify(docs, undefined, 2));
+                console.log('### Read-2 done ###');
+            }, (err) => {
+                console.log('Unable to find ', err);
+            });
+
+    // Update
+
     db.close();
 });
